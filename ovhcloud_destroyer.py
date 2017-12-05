5dc6985a5fb4b30936db62ebcecf58a81a56df40
Lukas Loesche <lukas@mesosphere.io>
2017-12-05 17:49:18 +0100

    Add volume cleanup to cloud destroyer script

--- ovhcloud_destroyer.py
+++ ovhcloud_destroyer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import ovh
 import sys
+import time
 from retrying import retry
 from multiprocessing.pool import ThreadPool
 
@@ -21,6 +22,48 @@ class OVHClient(ovh.Client):
 def delete_instance(instance_id):
     print('Requesting deletion of {}'.format(instance_id))
     c.delete('/cloud/project/{}/instance/{}'.format(project_id, instance_id))
+
+
+def detach_volume(volume_id):
+    print('Detaching volume {}'.format(volume_id))
+    try:
+        r = c.get('/cloud/project/{}/volume/{}'.format(project_id, volume_id))
+        for instance_id in r['attachedTo']:
+            print('Detaching volume {} from instance {}'.format(volume_id, instance_id))
+            r = c.post('/cloud/project/{}/volume/{}/detach'.format(project_id, volume_id),
+                       serviceName=project_id,
+                       instanceId=instance_id)
+        return True
+    except ovh.exceptions.APIError:
+        raise
+
+
+def cleanup_volume(volume_id):
+    print('Cleaning up volume {}'.format(volume_id))
+    detach_volume(volume_id)
+    wait_for_volume(volume_id, wait_status='available')
+    delete_volume(volume_id)
+
+
+def delete_volume(volume_id):
+    print('Removing volume {}'.format(volume_id))
+    c.delete('/cloud/project/{}/volume/{}'.format(project_id, volume_id))
+
+
+def wait_for_volume(volume_id, wait_status='in-use'):
+    wait = True
+    while wait:
+        wait = False
+        r = c.get('/cloud/project/{}/volume/{}'.format(project_id, volume_id))
+        if r['status'] == wait_status:
+            print('Volume {} is {}'.format(volume_id, r['status']))
+        elif r['status'] in ['attaching']:
+            print('Volume {} is still {}'.format(volume_id, r['status']))
+            wait = True
+        else:
+            print('Volume {} has an unexpected status {}'.format(volume_id, r['status']))
+        time.sleep(1)
+
 
 if len(sys.argv[1:]) == 0:
     print('Usage: {} <projectname>'.format(sys.argv[0]))
@@ -43,13 +86,17 @@ if not project_id:
     sys.exit(1)
 
 print('Fetching all instances')
-r = c.get('/cloud/project/{}/instance'.format(project_id))
+ri = c.get('/cloud/project/{}/instance'.format(project_id))
 
-if len(r) == 0:
-    print('Project {} has no running instances'.format(project))
+print('Fetching all volumes')
+rv = c.get('/cloud/project/{}/volume'.format(project_id))
+
+if len(ri) == 0 and len(rv) == 0:
+    print('Project {} has no running instances or volumes'.format(project))
     sys.exit(0)
 
-input('!!!WARNING - THIS WILL DESTROY ALL {} INSTANCES IN THE PROJECT {}!!!\nPress Enter to continue...'.format(
-    len(r), project))
+input('!!!WARNING - THIS WILL DESTROY ALL {} INSTANCES AND {} VOLUMES IN THE PROJECT {}!!!\nPress Enter to continue...'.format(
+    len(ri), len(rv), project))
 
-tp.map(delete_instance, [i['id'] for i in r])
+tp.map(cleanup_volume, [v['id'] for v in rv])
+tp.map(delete_instance, [i['id'] for i in ri])
